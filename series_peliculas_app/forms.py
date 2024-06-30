01da50be973d10ad55bce77eccee6af1614cbc28
mohammed sebti <mohammed.sebti@agqlabs.com>
2024-06-30 16:40:32 +0200

    mi segundo commit

--- series_peliculas_app/forms.py
+++ series_peliculas_app/forms.py
@@ -2,7 +2,8 @@ from typing import Any
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms 
-from .models import Registrar_serie_pelicula
+from .models import Registrar_serie_pelicula #, Genero
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class RegisterUserForm(UserCreationForm):
     email=forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
@@ -28,18 +29,22 @@ class RegistrarSeriePeliculaForm(forms.ModelForm):
         fields = '__all__'
         widgets = {
             'titulo_serie': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Titulo Serie','name':'titulo_serie'}),
-            'fecha_serie': forms.DateInput(attrs={'class': 'form-control', 'placeholder': 'Fecha Serie' , 'type': 'date'}),
-            'serie_o_pelicula': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Serie/Película', 'id':'id_serie_o_pelicula','name':'serie_o_pelicula'}),
+            #'fecha_serie': forms.DateInput(attrs={'class': 'form-control', 'placeholder': 'Fecha Serie' , 'type': 'date'}),
+            #'fecha_serie': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Año de la Serie', 'min': '1900', 'max': '2100', 'id':'id_fecha_serie'}),
+            'fecha_serie': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Año de la Serie', 'min': '1900', 'max': '2100', 'id':'id_fecha_serie'}),
+            #'serie_o_pelicula': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Serie/Película', 'id':'id_serie_o_pelicula','name':'serie_o_pelicula'}),
+            'serie_o_pelicula': forms.Select(attrs={'class': 'form-control'}, choices=Registrar_serie_pelicula.SERIE_PELICULA_CHOICES),
             'temporadas': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Nº Temporadas', 'id':'id_temporadas'}),
             'episodios': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Nº Episodios', 'id':'id_episodios'}),
             'plataforma': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Plataforma Serie','name':'plataforma'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Descripción Serie'}),
-            'genero': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Género Serie'}),
-            'puntuacion': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Puntuación Serie', 'min' : 1, 'max' : 10}),
+            #'genero': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Género Serie'}),
+            'genero': forms.Select(attrs={'class': 'form-control'}, choices=Registrar_serie_pelicula.GENERO_CHOICES),
+            #'puntuacion': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Puntuación Serie', 'min' : 1, 'max' : 10}),
+            'puntuacion': forms.Select(attrs={'class': 'form-control'}, choices=Registrar_serie_pelicula.PUNTUACION_CHOICES),
             'finalizada': forms.CheckboxInput(attrs={'class': 'form-check-input','id':'id_finalizada'}),
             'comentarios': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Comentarios Serie'}),
             'serie_pelicula_imagen': forms.ClearableFileInput(attrs={'class': 'form-control', 'name':'files' ,'id':'formFile', 'type':'file'}),
-           
             'recomendado_por': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Recomendado Por'}),
         }
         def __init__(self, *args, **kwargs):
--- series_peliculas_app/models.py
+++ series_peliculas_app/models.py
@@ -7,21 +7,54 @@ from datetime import date
 # models.py
 from django.db import models
 from datetime import date
+from django import forms
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Registrar_serie_pelicula(models.Model):
-    titulo_serie = models.CharField('Titulo Serie', max_length=120)
-    fecha_serie = models.DateField('Fecha Serie')
-    serie_o_pelicula = models.CharField('Serie/Pelicula', max_length=120)
+    SERIE_PELICULA_CHOICES = [
+        ('Serie', 'Serie'),
+        ('Pelicula', 'Película')
+    ]
+    GENERO_CHOICES = [
+        ('Drama', 'Drama'),
+        ('Comedia', 'Comedia'),
+        ('Ciencia ficción', 'Ciencia ficción'),
+        ('Fantasía', 'Fantasía'),
+        ('Acción', 'Acción'),
+        ('Terror', 'Terror'),
+        ('Aventura', 'Aventura'),
+        ('Thriller', 'Thriller'),
+        ('Policial', 'Policial'),
+        ('Histórica', 'Histórica'),
+        ('Animación', 'Animación'),
+        ('Romance', 'Romance'),
+        ('Documental', 'Documental'),
+        ('Musical', 'Musical'),
+        ('Western', 'Western'),
+        ('Guerra', 'Guerra'),
+         ('Biográfica', 'Biográfica'),
+        ('Cine negro', 'Cine negro'),
+        ('Cine experimental:', 'Cine experimental:'),
+        ('Cine de arte', 'Cine de arte')
+    ]
+    
+    PUNTUACION_CHOICES = [(i, str(i)) for i in range(1, 11)]
+    serie_o_pelicula = models.CharField('Serie/Pelicula', max_length=120, choices=SERIE_PELICULA_CHOICES)
+    titulo_serie = models.CharField('Titulo', max_length=120)
+    fecha_serie = models.IntegerField('Año')
+    #serie_o_pelicula = models.CharField('Serie/Pelicula', max_length=120)
+    #serie_o_pelicula = models.CharField('Serie/Pelicula', max_length=120, choices=SERIE_PELICULA_CHOICES)
     temporadas = models.IntegerField('Nº Temporadas',blank=True, null=True)
     episodios = models.IntegerField('Nº Episodios',blank=True, null=True)
-    plataforma = models.CharField('Plataforma Serie', max_length=120)
-    description = models.TextField('Description Serie', blank=True)
-    genero = models.CharField('Género Serie', max_length=120)
-    puntuacion = models.IntegerField('Puntuacion Serie (Del 1 al 10)')
+    plataforma = models.CharField('Plataforma', max_length=120)
+    description = models.TextField('Description', blank=True)
+    #genero = models.CharField('Género', max_length=120)
+    genero = models.CharField('Género', max_length=120, choices=GENERO_CHOICES)
+    #puntuacion = models.IntegerField('Puntuacion Serie (Del 1 al 10)')
+    puntuacion = models.IntegerField('Puntuacion (Del 1 al 10)', choices=PUNTUACION_CHOICES)
     finalizada = models.BooleanField('Finalizada?', default=False)
-    comentarios = models.TextField('Comentarios Serie', blank=True)
+    comentarios = models.TextField('Comentarios', blank=True)
     serie_pelicula_imagen = models.ImageField(null=True, blank=True, upload_to="images/")
-   
     recomendado_por = models.CharField('Recomendado Por', max_length=120)
 
     def __str__(self) -> str:
